5a607e86c7ed95ff6565d45e8fe669e9d72b3306
SamayaA <asadullaevasamaya@gmail.com>
2022-06-17 20:26:14 +0300

    Timer. SetTimeout doesn't work properly

--- timer/task.js
+++ timer/task.js
@@ -1,1 +1,22 @@
+
 const timer = document.getElementById( 'timer' );
+
+function delayTime(time){
+    setTimeout(timerSet(time), 1000);
+}
+
+function timerSet(value) {
+    if (value > 0){
+        timer.textContent = timer.textContent - 1;
+    }
+    else {
+        alert("You win");
+    }
+}
+
+while (timer.textContent > 0){
+    delayTime(timer.textContent);
+    console.log(timer.textContent);
+}
+    
+
