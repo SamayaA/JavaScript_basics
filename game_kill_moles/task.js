ab0efea9aa2887b100e0d26b84e835ab6c244979
SamayaA <asadullaevasamaya@gmail.com>
2022-06-19 19:29:01 +0300

    v1. Game Kill Moles

--- game_kill_moles/task.js
+++ game_kill_moles/task.js
@@ -1,23 +1,31 @@
 const dead = document.getElementById("dead");
 const lost = document.getElementById("lost");
 
+function clear(){
+    dead.innerHTML = 0;
+    lost.innerHTML = 0;
+
+}
 function hasMole(hole){
     if (hole.classList.contains("hole_has-mole")){
         dead.innerHTML = parseInt(dead.innerHTML) + 1;
         // check for the win
         if (dead.innerHTML == 10){
-            alert("Победа!")
+            alert("Победа!");
+            clear();
         }
     }
     else {
-        lost.innerHTML = parseInt(dead.innerHTML) + 1;
+        lost.innerHTML = parseInt(lost.innerHTML) + 1;
         // check for lose
         if (lost.innerHTML == 5){
-            alert("Вы проиграли!")
+            alert("Вы проиграли!");
+            clear();
         }
     }
 } 
 
-for (i=1; i<=9; i++){
-    document.getElementById(`hole${i}`).onclick = hasMole(document.getElementById(`hole${i}`));
+for (i = 1; i <= 9; i++) {
+    let element = document.getElementById(`hole${i}`);
+    element.onclick = () => {hasMole(element)};
 }
