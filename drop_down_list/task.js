6097ce2855f633e46beb776e1523fb7342ebb12b
SamayaA <asadullaevasamaya@gmail.com>
2022-06-29 21:10:41 +0300

    DropDown list return false onclick

--- drop_down_list/task.js
+++ drop_down_list/task.js
@@ -1,20 +1,21 @@
+
 const dropDown = document.querySelector(".dropdown");
 const dropDownClassList = document.querySelector(".dropdown__list").classList;
 const value = document.querySelector(".dropdown__value");
 
 const listItems = Array.from(document.querySelectorAll(".dropdown__item"));
 
-
 function list() {
     dropDownClassList.toggle("dropdown__list_active");
+    return false;
 }
 
 // change value of dropDown 
 function changeValue(evt) {
     value.innerHTML = evt.target.textContent;
     list();
+    return false;
 }
-
 
 dropDown.addEventListener('click', list);
 
