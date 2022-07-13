9962bc8c33e970d454b03e020ddc8784197809a5
SamayaA <asadullaevasamaya@gmail.com>
2022-07-13 21:59:12 +0300

    Added border to active font and fixed font switching

--- book_reader/task.js
+++ book_reader/task.js
@@ -1,19 +1,22 @@
 const book = document.querySelector(".book")
 const fontSizes = Array.from(document.querySelectorAll(".font-size"));
-const activeFont = document.querySelector(".font-size_active");
 
 function changeFont(event) {
     event.preventDefault();
+    book.className = "book";
+
+    let activeFont = document.querySelector(".font-size_active");
+    activeFont.classList.remove("font-size_active");
+    event.target.classList.add("font-size_active");
+
     if (event.target.dataset.size == "small"){
-        book.classList.add("book_fs-small");
+        book.classList.add("book_fs-small");        
         return false
     };
     if (event.target.dataset.size == "big"){
         book.classList.add("book_fs-big");
         return false
     };
-    book.className = "book";
-    return false
 }
 
 fontSizes.forEach((element) => {
