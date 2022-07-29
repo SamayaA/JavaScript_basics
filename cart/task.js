70ad41413a9a8bbd8e537207a7bcfea03d722bb2
SamayaA <asadullaevasamaya@gmail.com>
2022-07-29 20:52:04 +0300

    Refactor code and optimize memory usage of localStorage

--- cart/task.js
+++ cart/task.js
@@ -1,23 +1,12 @@
 const cart = document.querySelector(".cart");
-
 const cartProducts = document.querySelector(".cart__products");
-cartProducts.innerHTML = (localStorage.getItem("cartProducts") !== null)? localStorage.getItem("cartProducts"): cartProducts.innerHTML;
+const cartProductsList = (localStorage.getItem("cartProducts") !== null)? JSON.parse(localStorage.getItem("cartProducts")): [];
 
 const btnAdd = Array.from(document.querySelectorAll(".product__add"));
 
 const productInc = Array.from(document.querySelectorAll(".product__quantity-control_inc"));
 const productDec = Array.from(document.querySelectorAll(".product__quantity-control_dec"));
 
-// remove signs from localStorage
-const removeSigns = Array.from(document.querySelectorAll(".cart__product__remove"));
-removeSigns.forEach((element)=> {
-    element.addEventListener("click", removeProductCart)
-})
-
-// display cart if it is not empty
-if (cartProducts.children.length > 0){
-    cart.style.display = "block";
-}
 
 // change quantity of product
 function changeQuantity(event, number){
@@ -37,54 +26,84 @@ function findProductInCart(productId){
 }
 
 function removeProductCart(event){
-    console.log(event.target.closest(".cart__product"))
-    cartProducts.removeChild(event.target.closest(".cart__product"));
-    localStorage.setItem("cartProducts", cartProducts.innerHTML);
+    let product = event.target.closest(".cart__product");
+    cartProducts.removeChild(product);
+    let productIndex = cartProductsList.indexOf(cartProductsList.find((item)=> item.id == product.dataset.id));
+    cartProductsList.splice(productIndex, 1);
+    localStorage.setItem("cartProducts", JSON.stringify(cartProductsList));
     // cart display
-    cart.style.display = (cartProducts.children.length > 0)? "block": "none";
+    if (cartProducts.children.length > 0){
+        cart.style.display = "block";
+    } else {
+        localStorage.removeItem("cartProducts");
+        cart.style.display = "none";
+    }
     event.stopPropagation();
     event.preventDefault();
+}
+
+function appendProduct(id, src, count){
+    let product = document.createElement("div");
+    product.setAttribute("class", "cart__product");
+    product.setAttribute("data-id", id);
+    
+    let img = document.createElement("img");
+    img.setAttribute("class", "cart__product-image");
+    img.setAttribute("src", src)
+    product.append(img);
+    // product quantity
+    let productCount = document.createElement("div");
+    productCount.className = "cart__product-count";
+    productCount.innerText = count;
+    product.append(productCount);
+    // remove sign
+    let removeSign = document.createElement("a");
+    removeSign.innerHTML = "&times";
+    removeSign.href = "#";
+    removeSign.className = "cart__product__remove";
+    removeSign.addEventListener("click", removeProductCart)
+    product.append(removeSign);
+    cartProducts.append(product);
 }
 
 function addProductToCart(event){
     // cart display
     cart.style.display = "block";
-    // produc creation
-    let product = event.target.closest(".product").cloneNode(false);
-    product.className = "cart__product";
-    let productOfID = findProductInCart(product.dataset.id)
-    let count = event.target.parentElement.querySelector(".product__quantity-value");
+    let id = event.target.closest(".product").dataset.id;
+    let productOfID = findProductInCart(id);
+    let count = event.target.closest(".product__quantity").querySelector(".product__quantity-value").innerText.trim();
     // if product isn't in cart
     if ( productOfID == undefined){
-        let img = event.target.closest(".product").querySelector(".product__image").cloneNode(true);
-        img.className = "cart__product-image"
-        product.append(img);
-
-        if (count !== null){
-            // product quantity
-            let productCount = document.createElement("div");
-            productCount.className = "cart__product-count";
-            productCount.innerText = count.innerText;
-            product.append(productCount);
-            // remove sign
-            let removeSign = document.createElement("a");
-            removeSign.innerHTML = "&times";
-            removeSign.href = "#";
-            removeSign.className = "cart__product__remove";
-            removeSign.addEventListener("click", removeProductCart)
-            product.append(removeSign);
-
-            cartProducts.append(product);
-        }
-    } else {
+        let src = event.target.closest(".product").querySelector(".product__image").src;
+        // produc creation
+        appendProduct(id, src, count);
+        let productInf = {};
+        productInf["id"] = id;
+        productInf["src"] = src;
+        productInf["count"] = count;
+        cartProductsList.push(productInf);
+    } 
+    else {
         let value = productOfID.querySelector(".cart__product-count");
-        value.innerText = `${parseInt(value.innerText) + parseInt(count.innerText)}`
+        value.innerText = `${parseInt(value.innerText) + parseInt(count)}`;
+        let productIndex = cartProductsList.indexOf(cartProductsList.find((item)=> item.id == id));
+        cartProductsList[productIndex].count = `${parseInt(value.innerText)}`;      
     }
     count.innerText = '1';
-    localStorage.setItem("cartProducts", cartProducts.innerHTML);
+
+    localStorage.setItem("cartProducts", JSON.stringify(cartProductsList));
 }
 
 
+// add products to cart from localStorage
+cartProductsList.forEach(element => {
+    appendProduct(element.id, element.src, element.count)
+})
+
+// display cart if it is not empty
+if (cartProducts.children.length > 0){
+    cart.style.display = "block";
+}
 productInc.forEach((element) => {
     element.addEventListener("click", (event) =>{
         changeQuantity(event, 1);
