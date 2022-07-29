426dc5623a1eaadbd65b6493b1d8f7652c664a49
SamayaA <asadullaevasamaya@gmail.com>
2022-07-29 19:31:22 +0300

    refactor the code and decrease localStorage memory usage

--- todo/task.js
+++ todo/task.js
@@ -1,51 +1,57 @@
 
 const tasksList = document.getElementById("tasks__list");
-tasksList.innerHTML = (localStorage.getItem("tasksList") == null)? document.getElementById("tasks__list").innerHTML: localStorage.getItem("tasksList");
+const toDos = (localStorage.getItem("tasksList") !== null)? JSON.parse(localStorage.getItem("tasksList")): [];
 const inputTask = document.getElementById("task__input");
 const btn = document.getElementById("tasks__add");
 
-// for localStorage data add EventListener
-const removeTasks = Array.from(document.querySelectorAll(".task__remove"));
-removeTasks.forEach((element)=>{
-    if (element != null){
-        element.addEventListener("click", (event)=>{
-            event.target.closest(".task").remove();
-            localStorage.removeItem("tasksList");
-            localStorage.setItem("tasksList", document.getElementById("tasks__list").innerHTML);
-        });
-    }
-})
+function createTask(value=inputTask.value) {
+    let task = document.createElement("div");
+    task.setAttribute("class", "task");
+
+    let taskTitle = document.createElement("div");
+    taskTitle.setAttribute("class", "task__title");
+    taskTitle.textContent = value;
+    task.append(taskTitle);
+
+    let a = document.createElement("a");
+    a.setAttribute("href", "#");
+    a.setAttribute("class", "task__remove");
+    a.innerHTML = "&times;";
+    task.append(a);
+    removeTask(task);
+    tasksList.append(task);
+}
 
 function removeTask(task){
     let removeSign = task.querySelector(".task__remove");
     if (removeSign != null){
         removeSign.addEventListener("click", (event)=>{
             event.target.closest(".task").remove();
+            let taskIndex = toDos.indexOf(task.querySelector(".task__title").textContent.trim())
+            toDos.splice(taskIndex, 1);
             localStorage.removeItem("tasksList");
-            localStorage.setItem("tasksList", document.getElementById("tasks__list").innerHTML);
+            localStorage.setItem("tasksList", JSON.stringify(toDos));
         });
     }
-    localStorage.setItem("tasksList", document.getElementById("tasks__list").innerHTML);
+    localStorage.setItem("tasksList", JSON.stringify(toDos));
 }
 
 
-function createTask() {
-    let task = document.createElement("div");
-    task.setAttribute("class", "task");
-    task.innerHTML = `
-    <div class="task__title">
-        ${inputTask.value}
-    </div>
-    <a href="#" class="task__remove">&times;</a> 
-    `;
-    tasksList.append(task);
+// add tasks from localStorage
+toDos.forEach((element => {
+    createTask(element);
+}))
+
+// add task from input
+function appendTask() {
+    toDos.push(inputTask.value.trim());
+    createTask();
     inputTask.value = '';
-    removeTask(task);
 }
 
 createTaskElement = (event) => {
     if (inputTask.value.trim().length > 1){
-        createTask();
+        appendTask();
         event.stopPropagation();
         event.preventDefault();
     } 
